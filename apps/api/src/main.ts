685179cd50e988cc6167619671f12abbbab9b1b1
rlebhar-ext <rlebhar-ext@klemo.com>
2026-04-01 19:55:29 +0200

    fix urls

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -29,10 +29,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
     cors: {
-      // origin: [process.env.FRONTEND_URL as string, process.env.MOBILE_URL_WEB as string, process.env.MOBILE_URL_EMULATOR as string, "192.168.1.224", "http://192.168.1.224:3001"],
-      // credentials: true,
-      origin: "*",
+      origin: [process.env.FRONTEND_URL as string, process.env.MOBILE_URL_WEB as string, process.env.MOBILE_URL_EMULATOR as string, "192.168.1.224", "http://192.168.1.224:3001"],
       credentials: true,
+      // origin: "*",
+      // credentials: true,
     },
   });
   app.useGlobalPipes(new ValidationPipe());
